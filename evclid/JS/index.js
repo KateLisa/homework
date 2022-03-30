8a6fd1b16f18e1c9ac79393d52c7057b3871343c
Kate <Kate@>
2022-03-30 12:46:40 +0400

    aded swiper

--- evclid/JS/index.js
+++ evclid/JS/index.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const swiper = new Swiper('.swiper', {
         // Optional parameters
-        direction: 'vertical',
+        direction: 'horizontal',
         loop: true,
 
         // If we need pagination
@@ -11,15 +11,5 @@ window.addEventListener('DOMContentLoaded', function() {
             el: '.swiper-pagination',
         },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
     });
 })
